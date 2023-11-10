a2ef3ac78335e4105282df8b2a01bae5cd4e0030
Saad <you@example.com>
2023-11-10 20:18:38 +0000

    Double checked the codes

--- milestone_5.py
+++ milestone_5.py
@@ -1,17 +1,17 @@
 import random
 
 class Hangman:
-    # Step 1: Create an __init__ method with word_list and num_lives as parameters
+    
     def __init__(self, word_list, num_lives=5):
-        # Step 2: Initialize attributes
+       
         self.word_list = word_list
         self.num_lives = num_lives
         self.list_of_guesses = []
-        # Pick a random word from word_list
+        
         self.word = random.choice(self.word_list)
-        # Initialize word_guessed with '_' for each letter not yet guessed
+       
         self.word_guessed = ['_' for _ in range(len(self.word))]
-        # Calculate the number of unique letters in the word
+        
         self.num_letters = len(set(self.word))
 
     def check_guess(self, guess):
